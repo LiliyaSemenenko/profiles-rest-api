02e2618bd2c1d67331a16638fd86e6c6e08c903f
Liliya Semenenko <68308945+LiliyaSemenenko@users.noreply.github.com>
2023-06-11 17:59:27 -0700

    Added STATIC_ROOT

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+### added myself
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ### tells Django to look in the profiles_api and find UserProfile
 # and use it for all the authentification and user registration
