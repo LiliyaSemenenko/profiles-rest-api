22a379141ab1ff2a5cf279ba654d21898fb23eed
Liliya Semenenko <68308945+LiliyaSemenenko@users.noreply.github.com>
2023-06-11 18:30:29 -0700

    Modified settings.py

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,10 +3,10 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/LiliyaSemenenko/profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/LondonAppDeveloper/byob-profiles-rest-api.git'
 
-# dir we'll store our project on a server
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps'
+VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
@@ -14,35 +14,79 @@ locale-gen en_GB.UTF-8
 # Install Python, SQLite and pip
 echo "Installing dependencies..."
 apt-get update
-# nginx: the webserver that is going to serve the static files and act as a proxy to our uWSGI service 
-# that is going to run in supervisor
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
-# Create project directory
 mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/profiles-rest-api
 
-# Create virtual environment
-python3 -m venv $PROJECT_BASE_PATH/env
+mkdir -p $VIRTUALENV_BASE_PATH
+python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 
-# Install Python packages within requirments.txt file
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
 
 # Run migrations
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
+cd $PROJECT_BASE_PATH/profiles-rest-api/src
 
 # Setup Supervisor to run our uwsgi process.
-# Supervisor: app on Linux that allows you to manage processes (Python)
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Setup nginx to make our application accessible.
-# nginx web server is used to serve the static files
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
 
 echo "DONE! :)"
+
+###################################################################
+# #!/usr/bin/env bash
+
+# set -e
+
+# # TODO: Set to URL of git repo.
+# PROJECT_GIT_URL='https://github.com/LiliyaSemenenko/profiles-rest-api.git'
+
+# # dir we'll store our project on a server
+# PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+
+# # Set Ubuntu Language
+# locale-gen en_GB.UTF-8
+
+# # Install Python, SQLite and pip
+# echo "Installing dependencies..."
+# apt-get update
+# # nginx: the webserver that is going to serve the static files and act as a proxy to our uWSGI service 
+# # that is going to run in supervisor
+# apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+
+# # Create project directory
+# mkdir -p $PROJECT_BASE_PATH
+# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
+# # Create virtual environment
+# python3 -m venv $PROJECT_BASE_PATH/env
+
+# # Install Python packages within requirments.txt file
+# $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+
+# # Run migrations
+# $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
+
+# # Setup Supervisor to run our uwsgi process.
+# # Supervisor: app on Linux that allows you to manage processes (Python)
+# cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+# supervisorctl reread
+# supervisorctl update
+# supervisorctl restart profiles_api
+
+# # Setup nginx to make our application accessible.
+# # nginx web server is used to serve the static files
+# cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+# rm /etc/nginx/sites-enabled/default
+# ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+# systemctl restart nginx.service
+
+# echo "DONE! :)"
