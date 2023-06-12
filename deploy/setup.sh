d473658dbdcedf6b9dac152180b172449fa2c895
Liliya Semenenko <68308945+LiliyaSemenenko@users.noreply.github.com>
2023-06-11 21:18:14 -0700

    Replaced link in setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,7 +1,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/LondonAppDev/profiles-rest-api.git'
+PROJECT_GIT_URL='https://github.com/LiliyaSemenenko/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
