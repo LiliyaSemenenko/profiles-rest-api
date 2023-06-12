48311d413ad5bc1f7c6a1b1a6fac577af8b9157c
Liliya Semenenko <68308945+LiliyaSemenenko@users.noreply.github.com>
2023-06-11 21:32:05 -0700

    Changed Run migrations section in setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -23,7 +23,9 @@ python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 $VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
 
 # Run migrations
-cd $PROJECT_BASE_PATH/profiles-rest-api/src
+# cd $PROJECT_BASE_PATH/profiles-rest-api/src
+### !!!!
+$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
 # Setup Supervisor to run our uwsgi process.
 cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
